9ad7e05ae1532367144c27639fa1a69af891520a
GameGeniX <83692530+GameGeniX@users.noreply.github.com>
2021-06-12 14:36:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+    var options ={
+        restitution:0.3
+        friction:5
+    }
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+
+  
+
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+
+    
+ 
+}
